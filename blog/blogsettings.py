6ab5056c979b61053400c8363fa172da079e5b73
Maikel Chandika <mkdika@gmail.com>
2018-08-14 12:16:30 +0700

    add favicon

--- blog/blogsettings.py
+++ blog/blogsettings.py
@@ -14,18 +14,18 @@ class BlogSetting():
 
     about_sub_title = "How's this blog goes"
     about_blog = """
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus orci est, 
-aliquet a mi sit amet, mollis imperdiet enim. Aenean luctus, libero at eleifend rutrum, 
-velit nunc finibus neque, vel posuere augue nisi ac eros. Quisque lacinia metus 
-sed rutrum porttitor. Ut bibendum sit amet ligula vitae egestas. Cras pellentesque 
-ligula lorem, sit amet ullamcorper metus feugiat eu. Proin aliquam tortor arcu, 
-sed egestas nibh cursus sit amet. Curabitur et aliquet nisl, a vehicula ex. Duis
- sed facilisis tortor.</p>
-<p>Nam luctus dapibus dignissim. Vestibulum blandit sit amet sapien nec venenatis. 
-Phasellus eget cursus lorem. Sed vehicula lacinia vehicula. Morbi non dolor et libero 
-dapibus aliquam vel ac arcu. Ut vitae pellentesque nisi. Donec lobortis posuere posuere. 
-Maecenas accumsan consequat auctor. Ut in leo nec tortor lacinia consectetur vitae 
-id orci. Praesent euismod ornare risus sed placerat. Etiam at risus elit. Vivamus 
-quis suscipit felis. Morbi dictum pellentesque egestas. Proin at felis et orci 
-tempor efficitur sed at augue.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus orci est, 
+                aliquet a mi sit amet, mollis imperdiet enim. Aenean luctus, libero at eleifend rutrum, 
+                velit nunc finibus neque, vel posuere augue nisi ac eros. Quisque lacinia metus 
+                sed rutrum porttitor. Ut bibendum sit amet ligula vitae egestas. Cras pellentesque 
+                ligula lorem, sit amet ullamcorper metus feugiat eu. Proin aliquam tortor arcu, 
+                sed egestas nibh cursus sit amet. Curabitur et aliquet nisl, a vehicula ex. Duis
+                sed facilisis tortor.</p>
+                <p>Nam luctus dapibus dignissim. Vestibulum blandit sit amet sapien nec venenatis. 
+                Phasellus eget cursus lorem. Sed vehicula lacinia vehicula. Morbi non dolor et libero 
+                dapibus aliquam vel ac arcu. Ut vitae pellentesque nisi. Donec lobortis posuere posuere. 
+                Maecenas accumsan consequat auctor. Ut in leo nec tortor lacinia consectetur vitae 
+                id orci. Praesent euismod ornare risus sed placerat. Etiam at risus elit. Vivamus 
+                quis suscipit felis. Morbi dictum pellentesque egestas. Proin at felis et orci 
+                tempor efficitur sed at augue.</p>
                   """
